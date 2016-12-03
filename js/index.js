223f524d6d4ac3029e0dfed9dcdee1019472f0d3
jesusdrodriguez <jesus.d.rodriguez01@utrgv.edu>
2016-12-03 14:02:35 -0600

    finalizing app

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+var footerResize = function() {
+  $('#footer').css('position', $("body").height() + $("#footer").innerHeight() > $(window).height() ? "inherit" : "fixed");
+};
+$(window).resize(footerResize).ready(footerResize);
